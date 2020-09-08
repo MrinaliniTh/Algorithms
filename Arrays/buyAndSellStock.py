6184ccb2034879a83469054eb8c92ef461021de1
Devi <thokchom.m.devi@boeing.com>
2020-09-09 00:49:43 +0530

    bus and sale stock

--- Arrays/buyAndSellStock.py
+++ Arrays/buyAndSellStock.py
@@ -6,7 +6,7 @@
 # maintain two variable initially i.e, sale_stock and buy_stock
 # Iterate over prices
 # buy_stock will be assigned as infinity at the begining
-# sale_stock will be at the begining
+# sale_stock will be 0 at the begining
 # if price is less than buy stock, replace buy stock with the price
 # else replace sale_stock with max of (sale_stock, price - buy_stock)
 # return sale_stock
