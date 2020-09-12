8234bdc6b945a31d35d2727cdf9b365d2c856790
Devi <thokchom.m.devi@boeing.com>
2020-09-12 17:25:32 +0530

    reverse a string using recursion

---
+++ Recursion/reverse_string.py
@@ -0,0 +1,10 @@
+def reverse_string(string:str)-> str:
+    size = len(string)
+    if size == 0:
+        return
+    last_char = string[size - 1]
+    print(last_char, end = "")
+    reverse_string(string[0:size - 1])
+
+
+reverse_string('GOOGLE')
