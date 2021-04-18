d1c5bc9b7975f83a1ce6b05a72a7b65f267cd39e
Devi <thokchom.m.devi@boeing.com>
2021-04-18 21:25:39 +0530

    another approach of path sum

--- tree/binary_tree/path_sum_112.py
+++ tree/binary_tree/path_sum_112.py
@@ -12,6 +12,17 @@ class BinaryTree:
             return True
         return self.path_sum(root.left, target-root.val) or self.path_sum(root.right, target-root.val)
 
+    def path_sum_2(self, root, target):
+        return self.path_sum_res(root, target, 0)
+
+    def path_sum_res(self, root, target, sum):
+        if not root:
+            return False
+        sum += root.val
+        if sum == target and not root.left and not root.right:
+            return True
+        return self.path_sum_res(root.left, target, sum) or self.path_sum_res(root.right, target, sum)
+
 tree = Node(1)
 tree.left = Node(2)
 tree.right = Node(3)
@@ -20,4 +31,4 @@ tree.left.right = Node(5)
 tree.right.left = Node(6)
 tree.right.right = Node(7)
 binary_tree = BinaryTree()
-print(binary_tree.path_sum(tree, 8))
+print(binary_tree.path_sum_2(tree, 22))
