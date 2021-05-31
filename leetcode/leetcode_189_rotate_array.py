dab02cc64f5622b027d6c8c007d2ba27e61e3079
Devi <thokchom.m.devi@boeing.com>
2021-05-31 22:54:49 +0530

    leet

---
+++ leetcode/leetcode_189_rotate_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums, k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+
+    def reverse(self, nums, low, high):
+        while low < high:
+            nums[low], nums[high] = nums[high], nums[low]
+            low += 1
+            high -= 1
